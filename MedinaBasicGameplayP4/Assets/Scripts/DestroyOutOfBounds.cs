c971846667858faee87990b8f18bb745ee4978d1
arielle777 <723653@student.dusd.net>
2025-03-28 10:15:59 -0700

    Completed

--- MedinaBasicGameplayP4/Assets/Scripts/DestroyOutOfBounds.cs
+++ MedinaBasicGameplayP4/Assets/Scripts/DestroyOutOfBounds.cs
@@ -10,7 +10,7 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,10 +20,11 @@ public class DestroyOutOfBounds : MonoBehaviour
         {
             Destroy(gameObject);
         }
-      else if ( transform.position.z < lowerBound) 
+        else if (transform.position.z < lowerBound)
         {
+            Debug.Log("GAME OVER");
             Destroy(gameObject);
         }
-        
+
     }
 }
