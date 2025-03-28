7944a0cc11ec86af2fd70f2207a6d9bd43897dda
arielle777 <723653@student.dusd.net>
2025-03-28 11:44:39 -0700

    Completed first bonus challenge
    Done with creating vertical movement and bounaries

--- MedinaBasicGameplayP4/Assets/Scripts/PlayerController.cs
+++ MedinaBasicGameplayP4/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput; 
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public float zRange = -11.0f;
 
     public GameObject projectilePrefab;
 
@@ -30,13 +32,29 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.z < -12)
+        {
+            transform.position = new Vector3(-11,0, 0);
+        }
+        if (transform.position.z > 6)
+        {
+            transform.position = new Vector3(-11, 0, 0);
+        }
+
+        // Get player to move 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+       
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward *  verticalInput * Time.deltaTime * speed);
+
+       
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a projectile from the player 
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
         }
     }
     }
